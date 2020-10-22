1803582a0acce9013f237b0e9bbf49a8693ccf8e
caiovini980 <caiovini.aa@gmail.com>
2020-10-22 19:07:12 -0300

    Added the player controller, the bouncing effect, the teleportation if it passes through the sides and some animations for the player

---
+++ Jumper/Assets/Scripts/PlayerBoostUp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoostUp : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+
+    [SerializeField] private bool canJump;
+    private Animator animator;
+    private float speed = 5f;
+
+    //Animations
+    private string PLAYER_JUMP = "isJumping";
+    private string PLAYER_TAKEOFF = "takeOff";
+
+    public int jumpCount;
+
+    private void Awake()
+    {
+        animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        playerRb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpCount = 0;
+        if (!playerRb)
+            return;
+
+        canJump = false;
+    }
+
+    private void Update()
+    {
+        if (canJump && playerRb.velocity.y <= 0)
+        {
+            animator.SetTrigger(PLAYER_TAKEOFF);
+            playerRb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+            jumpCount++;
+            canJump = false;
+        }
+        animator.SetBool(PLAYER_JUMP, false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+        animator.SetBool(PLAYER_JUMP, true);
+        Debug.Log("Collision detected");
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        animator.SetBool(PLAYER_JUMP, false);
+    }
+
+}
---
+++ Jumper/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.5f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!rb)
+            return;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > 3.0f)
+        {
+            transform.position = new Vector2(-3f, transform.position.y);
+        }
+        else if (transform.position.x < -3.0f)
+        {
+            transform.position = new Vector2(3f, transform.position.y);
+        }
+        else if (transform.position.y < -5.0f)
+        {
+            transform.position = new Vector2(0f, 0f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).position.x > Screen.width / 2)
+            {
+                rb.AddForce(new Vector2(1 * speed * Time.deltaTime, 0));
+            }
+            else if (Input.GetTouch(0).position.x < Screen.width / 2)
+            {
+                rb.AddForce(new Vector2(-1 * speed * Time.deltaTime, 0));
+            }
+        }
+    }
+}
